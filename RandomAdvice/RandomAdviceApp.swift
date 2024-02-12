d0eb1a699035e0f182ef152a2df5ab7d6ce9a715
Cesario Nivar <ing.cesarionivar@gmail.com>
2024-02-12 11:58:38 -0500

    Initial Commit

---
+++ RandomAdvice/RandomAdviceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomAdviceApp.swift
+//  RandomAdvice
+//
+//  Created by Cesario Nivar on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct RandomAdviceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
