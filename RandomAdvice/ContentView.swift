c2e3019038a475ed5474304ccd40cfb4e19f8c2d
Cesario Nivar <ing.cesarionivar@gmail.com>
2024-02-12 12:23:36 -0500

    Create main view

--- RandomAdvice/ContentView.swift
+++ RandomAdvice/ContentView.swift
@@ -9,13 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color("backgroundColor")
+                .ignoresSafeArea()
+            
+            
+            VStack (spacing: 20) {
+                Text("Advice #118")
+                    .font(.title2.bold())
+                    .foregroundStyle(Color("textColor"))
+                
+                Text("A common regret in life is wishing one hadn't worked so hard.")
+                    .font(.system(size: 24))
+                    .foregroundStyle(Color("adviceColor"))
+                    .frame(maxHeight: .infinity)
+                    .padding(.bottom)
+            }
+            .padding()
+            .frame(width: 350, height: 250)
+            .background(Color("secondaryBackgroundColor"))
+            .clipShape(.rect(cornerRadius: 20))
+            
+            
+            Button {
+                
+                
+            } label: {
+                Image(systemName: "dice.fill")
+                    .font(.title2)
+                    .foregroundStyle(Color("backgroundColor"))
+            }
+            .padding()
+            .background(Color("textColor"))
+            .clipShape(Circle())
+            .offset(y: 120)
         }
-        .padding()
     }
 }
 
